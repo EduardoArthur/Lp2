0099d8ef5a92a7f783c3144ae0b289249beada4c
EduardoArthur <38742213+EduardoArthur@users.noreply.github.com>
2019-07-27 19:09:08 -0300

    Delete Ex2.sh

--- L2/E1/E2/Ex2.sh
+++
@@ -1,5 +0,0 @@
-echo $CLASSPATH
-export $CLASSPATH = ./lp2g12/biblioteca/Biblioteca.java; ./lp2g12/biblioteca/EmprestadoPara.java; ./lp2g12/biblioteca/Emprestimo.java; ./lp2g12/biblioteca/Livro.java; ./lp2g12/biblioteca/Pessoa.java; ./lp2g12/biblioteca/Usuario.java; ./lp2g12/exception/CopiaNaoDisponivelEx.java; ./lp2g12/exception/LivroNaoCadastradoEx.java; ./lp2g12/exception/NenhumaCopiaEmprestadaEx.java; ./lp2g12/exception/UsuarioNaoCadastradoEx.java;
-javac *java
-java Ex2
-unset CLASSPATH
