5bd69fcd62e10adc9e56a867d65d719f3233281e
EduardoArthur <38742213+EduardoArthur@users.noreply.github.com>
2019-07-27 19:09:37 -0300

    Delete NenhumaCopiaEmprestadaEx.java

--- L2/E1/E2/lp2g12/exception/NenhumaCopiaEmprestadaEx.java
+++
@@ -1,6 +0,0 @@
-package lp2g12.exception;
-public class NenhumaCopiaEmprestadaEx extends Exception{
-    public NenhumaCopiaEmprestadaEx(){
-        super("Nenhuma copia foi emprestada ainda");
-    }
-}
